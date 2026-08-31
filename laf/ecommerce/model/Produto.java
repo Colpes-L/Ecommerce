f7ae176f6c48c3ee389ae657cc992724056eac12
Luis <luiscolpes013@gmail.com>
2026-08-31 16:29:07 -0300

    refactor: criação de exceções no construtor da classe

--- laf/ecommerce/model/Produto.java
+++ laf/ecommerce/model/Produto.java
@@ -6,6 +6,15 @@ public class Produto {
     private double preco;
 
     public Produto(int id,String nome,double preco){
+        if(this.id <= 0){
+            throw new IllegalArgumentException("O id deve ser maior que 0.");
+        }
+        if(this.nome == null || this.nome.isBlank()){
+            throw new IllegalArgumentException("O nome do produto nao pode ser vazio.");
+        }
+        if(this.preco <= 0){
+            throw new IllegalArgumentException("O preco deve ser maior que 0.");
+        }
         this.id = id;
         this.nome = nome;
         this.preco = preco;
